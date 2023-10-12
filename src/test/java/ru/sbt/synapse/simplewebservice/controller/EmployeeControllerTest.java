6f9e002a3df79ec663341d690f7568f9706c618f
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2023-10-12 23:52:06 +0300

    Add EmployeeController test

---
+++ src/test/java/ru/sbt/synapse/simplewebservice/controller/EmployeeControllerTest.java
@@ -0,0 +1,58 @@
+package ru.sbt.synapse.simplewebservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.sbt.synapse.simplewebservice.property.Employee;
+import ru.sbt.synapse.simplewebservice.service.EmployeeServiceImpl;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EmployeeController.class)
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@DisplayName("Операции контроллера с events")
+class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private EmployeeServiceImpl service;
+
+    @Test
+    @DisplayName("Проверка получения пользователей")
+    void whenGet_thenReturns200() throws Exception {
+        mockMvc.perform(get("/api/v1/employees"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Проверка создания пользователя object")
+    void whenSendBodyAsObject_thenReturns200() throws Exception{
+        Employee employee = new Employee("Gor","Zerov","re@ya.ru");
+        mockMvc.perform(post("/api/v1/employees")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(employee)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Проверка создания пользователя curl")
+    void whenSendValuesAsCurlBody_thenReturns200() throws Exception {
+        mockMvc.perform(post("/api/v1/employees")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"firstName\":\"Artur\",\"secondName\":\"Gorov\",\"emailId\":\"ss@ya.ru\"}"))
+                .andExpect(status().isOk());
+    }
+
+}
