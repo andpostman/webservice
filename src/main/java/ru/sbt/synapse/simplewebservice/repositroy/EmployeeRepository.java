eab7681569ab4924b1b1626aebbc776f90333181
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2023-10-12 23:46:58 +0300

    Add DB repository to do queries

--- src/main/java/ru/sbt/synapse/simplewebservice/repositroy/EmployeeRepository.java
+++ src/main/java/ru/sbt/synapse/simplewebservice/repositroy/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package ru.sbt.synapse.simplewebservice.repositroy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.sbt.synapse.simplewebservice.model.Employee;
+import ru.sbt.synapse.simplewebservice.model.EmployeeDto;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<EmployeeDto, Long> {
 }
