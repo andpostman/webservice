d413170c4f5f35da282e0f3c0f9ca9ba5ab52fe9
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2023-10-19 18:02:42 +0300

    Add update and get rest endpoints

--- src/main/java/ru/sbt/synapse/simplewebservice/controller/EmployeeController.java
+++ src/main/java/ru/sbt/synapse/simplewebservice/controller/EmployeeController.java
@@ -1,8 +1,9 @@
 package ru.sbt.synapse.simplewebservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.sbt.synapse.simplewebservice.dto.Employee;
+import ru.sbt.synapse.simplewebservice.property.Employee;
 import ru.sbt.synapse.simplewebservice.model.EmployeeDto;
 import ru.sbt.synapse.simplewebservice.service.EmployeeServiceImpl;
 
@@ -11,18 +12,28 @@ import java.util.List;
 @CrossOrigin(origins = "${react.url}")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/employees")
 public class EmployeeController {
 
     private final EmployeeServiceImpl service;
 
-    @GetMapping("/employees")
+    @GetMapping
     public List<EmployeeDto> getAllEmployees(){
         return service.findAllEmployees();
     }
 
-    @PostMapping("/employees")
+    @PostMapping
     public EmployeeDto createEmployee(@RequestBody Employee employee){
         return service.saveEmployee(employee);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id")long id){
+        return ResponseEntity.ok(service.findEmployeeById(id));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") long id, @RequestBody Employee employee){
+        return ResponseEntity.ok(service.updateEmployee(id,employee));
     }
 }
