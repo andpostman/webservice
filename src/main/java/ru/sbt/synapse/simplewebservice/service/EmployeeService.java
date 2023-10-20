6ab1c029dfacde1f008683bb66ff8bec71dc8d55
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2023-10-21 00:36:10 +0300

    Add delete method to service

--- src/main/java/ru/sbt/synapse/simplewebservice/service/EmployeeService.java
+++ src/main/java/ru/sbt/synapse/simplewebservice/service/EmployeeService.java
@@ -4,12 +4,11 @@ import ru.sbt.synapse.simplewebservice.property.Employee;
 import ru.sbt.synapse.simplewebservice.model.EmployeeDto;
 
 import java.util.List;
-import java.util.Map;
 
 public interface EmployeeService {
     List<EmployeeDto> findAllEmployees();
     EmployeeDto saveEmployee(Employee employee);
     EmployeeDto findEmployeeById(long id);
     EmployeeDto updateEmployee(long id, Employee employee);
-    Map<String, Boolean> deleteEmployee(long id);
+    void deleteEmployee(long id);
 }
